514a42a976ac1b898ddf90a0dbca8b3518d10845
Mohamed9669 <kingmetzo1@gmail.com>
2025-02-13 11:01:56 +0000

    Definition de l'interface immobilier

---
+++ src/Immobilier.java
@@ -0,0 +1,7 @@
+
+public interface Immobilier {
+    public void copy(Immobilier immobilier);
+    public String toChaine();
+    public double convert();
+    public boolean egal(Immobilier immobilier);
+}
